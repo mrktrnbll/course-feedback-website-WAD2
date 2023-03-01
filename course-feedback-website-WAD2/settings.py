060199654460cdb0119c1d45d945af505a022f4a
Euan Marney <2619355m@student.gla.ac.uk>
2023-03-01 14:20:28 +0000

    Added picture model and media link

--- course-feedback-website-WAD2/settings.py
+++ course-feedback-website-WAD2/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -116,6 +116,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 
