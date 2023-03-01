7c27d2f74ade8c94ea79778819175b84107488ef
“Mark <2482252t@student.gla.ac.uk>
2023-03-01 15:00:12 +0000

    Sorted media file serving. cs1p.png can now be viewed.

--- course_feedback/urls.py
+++ course_feedback/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from course_feedback import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'course_feedback'
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('course/', views.course, name='course'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
