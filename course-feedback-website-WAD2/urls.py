a28056e2edc27e093047bf19889d8edca75b61cc
Alexandra Ulanovski <2635364u@student.gla.ac.uk>
2023-03-02 15:56:53 +0000

    adding more

--- course-feedback-website-WAD2/urls.py
+++ course-feedback-website-WAD2/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from course_feedback import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', include('course_feedback.urls')),
+    path('course_feedback/', include('course_feedback.urls')), 
     path('admin/', admin.site.urls),
-]
+
+]#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
