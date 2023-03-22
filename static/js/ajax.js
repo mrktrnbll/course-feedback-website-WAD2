89d8307122e0263e7b8e627d3e50448c82333bf6
“Mark <2482252t@student.gla.ac.uk>
2023-03-22 15:18:19 +0000

    Added upvote view for student accounts and for course pages.

---
+++ static/js/ajax.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('#like_btn').click(function() {
+		var catecategoryIdVar;
+		catecategoryIdVar = $(this).attr('data-categoryid');
+		$.get('/rango/like_category/',
+			{'category_id': catecategoryIdVar},
+			function(data) {
+				$('#like_count').html(data);
+				$('#like_btn').hide();
+			})
+	});
+});
--- static/js/scipt.js
+++
@@ -1,7 +0,0 @@
-
-let inputFile = document.getElementById('picture');
-let fileNameField = document.getElementById('file-name');
-inputFile.addEventListener('change', function(event)){
-	let uploadedFileName = event.target.files.[0].name;
-	fileNameField.textContent = uploadedFileName;
-}
